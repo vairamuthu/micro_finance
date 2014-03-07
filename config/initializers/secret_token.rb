dbe5340da6499f12f55f7561575cc48b9de1689c
vairamuthu <vairamuthu.it@gmail.com>
2014-03-08 00:01:25 +0530

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MicroFinance::Application.config.secret_key_base = 'a1ab160ba45f0f002c38fb4f6939347aa61ebdce9717c86527e9ea371b794d82a3280bf2192efc0329da19876c4b1a1f148b691a8fefd5c262be6f51ae6d772b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MicroFinance::Application.config.session_store :cookie_store, key: '_micro_finance_session'
